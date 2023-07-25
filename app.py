b48b8347c426f6a65846cf2af2c84f026edf9cc3
Xuecheng <leexuecheng2017@outlook.com>
2023-07-25 19:51:44 +0800

    del delay

--- app.py
+++ app.py
@@ -17,9 +17,6 @@ applications.get_swagger_ui_html = swagger_monkey_patch
 
 app = FastAPI(title='PDF拆分')
 
-async def add_delay():
-    time.sleep(1)
-
 @app.post('/pdf',tags=["执行完成后需要手动点击Download File下载拆分后的PDF文件，900页的pdf文件预计时间为20分钟左右！！"])
 async def ocr(background_tasks: BackgroundTasks,File: UploadFile = File(...,description='上传需要拆分的PDF'),delay: bool = Depends(add_delay)):
 
